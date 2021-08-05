314fcfbbab952f463d8b30f5c323e18b16e9d5a1
Jarki <yaroslav.ivashko@gmail.com>
2021-08-05 20:09:05 +0300

    now can add custom handlers for commands

--- core/emitter.py
+++ core/emitter.py
@@ -5,17 +5,33 @@ import logging
 class ContextDispatcher:
     def __init__(self):
         self.ee = EventEmitter()
-
-    def dispatch(self, context):
-        if 'message' in context.keys():
-            if 'text' in context["message"].keys():
-                if context["message"]["text"].split(' ')[0][0] == '/':
-                    logging.info("detected a command")
-                    self.ee.emit("command", context)
-                else:
-                    logging.info("detected a text message")
-                    self.ee.emit("text_message", context)
+        self.commands = []
 
     def get_emitter(self):
         return self.ee
+
+    def add_command(self, command: str):
+        """
+        add_command('start')
+        Adds a command for dispatcher to raise events on it
+        The event raised will be called name_command
+        (if we add command 'start', look for start_command event)
+        """
+        self.commands.append(command)
+
+    def dispatch(self, context):
+        if 'message' in context.keys():  # if it's a message
+            if 'text' in context["message"].keys():  # if the message contains text
+                if context["message"]["text"].split(' ')[0][0] == '/':  # if it starts with a '/',
+                    # it's considered a command
+                    if context["message"]["text"].split(' ')[0][1:] in self.commands:  # if it's a command set by user
+                        command_index = self.commands.index(context["message"]["text"].split(' ')[0][1:])
+                        logging.info(f"detected a command:{self.commands[command_index]}")
+                        self.ee.emit(self.commands[command_index], context)
+                    else:
+                        logging.info("detected an unknown command")
+                        self.ee.emit("command", context)
+                else:
+                    logging.info("detected a text message")
+                    self.ee.emit("text_message", context)
 
