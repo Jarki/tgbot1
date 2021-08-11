b5a27a4f6905c7125bc59f4aa81aeba09d1f5c16
Jarki <yaroslav.ivashko@gmail.com>
2021-08-12 00:36:10 +0300

    implemented stuff

--- core/main.py
+++ core/main.py
@@ -10,7 +10,7 @@ from telegram_pybot.bot import Bot
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token.token)
-bot.set_webhook("https://5982bd18516b.ngrok.io")
+bot.set_webhook("https://8318ff8ce251.ngrok.io")
 
 if len(allowed_chats) > 0:
     bot.ignore_disallowed_chats(True)
@@ -46,11 +46,36 @@ def respond_ban(context):
 
     db.ban_user(chat_id, user, name)
 
-    responses = ["Banned ", " is now banned", "Successfully banned "]
+    responses = ["Banned %user%", "%user% is now banned", "Successfully banned %user%"]
     bot.send_message(context, f"Banned {name}")
 
 
+def respond_stats_users(context):
+    chat_id = context["message"]["chat"]["id"]
+
+    string = "Top 10 bananas:\n"
+    users = db.get_table_users(chat_id)
+
+    for row in users:
+        string += f"{row.username} issued {row.counter} bans\n"
+
+    bot.send_message(context, string)
+
+
+def respond_stats_banned(context):
+    chat_id = context["message"]["chat"]["id"]
+
+    string = "Top 10 banned:\n"
+    users = db.get_table_banned(chat_id)
+
+    for row in users:
+        string += f"{row.username} got banned {row.counter} times\n"
+
+    bot.send_message(context, string)
+
+
 bot.add_command('ban', respond_ban)
+bot.add_command('stats_users', respond_stats_users)
+bot.add_command('stats_banned', respond_stats_banned)
 
 bot.run(server_config.host, server_config.port)
-
